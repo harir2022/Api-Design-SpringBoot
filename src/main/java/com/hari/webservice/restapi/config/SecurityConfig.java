7cabc7b932168f42792955e3c909438f40f5fe31
Hari Rajagopal <harirajagopalbtech@gmail.com>
2023-12-10 18:46:15 +0530

    JWT ;

--- src/main/java/com/hari/webservice/restapi/config/SecurityConfig.java
+++ src/main/java/com/hari/webservice/restapi/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.hari.webservice.restapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,8 +19,10 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth->auth.anyRequest().authenticated())
 			.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS	))
 			.httpBasic();
+		
+			http.headers().frameOptions().sameOrigin();
 			http.csrf(csrf -> csrf.disable());
-		;
+			http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 		return http.build();	
 	}
 }
