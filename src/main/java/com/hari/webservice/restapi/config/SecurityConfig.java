987db19014047093686addf91c65ef2b0561a822
Hari Rajagopal <harirajagopalbtech@gmail.com>
2023-12-14 10:49:59 +0530

    Oauth-Google

--- src/main/java/com/hari/webservice/restapi/config/SecurityConfig.java
+++ src/main/java/com/hari/webservice/restapi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hari.webservice.restapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -10,8 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 //@EnableMethodSecurity // for pre and post
-//@EnableMethodSecurity(jsr250Enabled = true)
-@EnableMethodSecurity(securedEnabled = true)
+@EnableMethodSecurity(jsr250Enabled = true)
+//@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
 	
@@ -26,13 +27,16 @@ public class SecurityConfig {
 						.anyRequest()
 						.authenticated())
 			.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS	))
-			.httpBasic();
+			.httpBasic();		
 			
-		
+						
+			http.oauth2Login(Customizer.withDefaults()); // oauth google
 		
 			http.headers().frameOptions().sameOrigin();
 			http.csrf(csrf -> csrf.disable());
 			http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+			
+			
 		return http.build();	
 	}
 }
